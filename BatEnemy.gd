ca311affbc2fe3e5c9f04be72f01bc43201c900f
ECorpron <ecorpron@gmail.com>
2020-10-03 17:54:24 -0700

    Cleaned up Bat spawn code

--- BatEnemy.gd
+++ BatEnemy.gd
@@ -1,31 +1,27 @@
 extends RigidBody2D
 
-# Declare member variables here. Examples:
+# Declares viable spawn box for bat
 var spawnxLeft = 150
 var spawnxRight = 1800
 var spawnyUp = 50
 var spawnyDown = 400
 
+# Finds range of acceptable spawns
 var spawnWidth = spawnxRight - spawnxLeft
 var spawnHeight = spawnyDown - spawnyUp
 
 var spawn = Vector2()
-var xLocation
-var yLocation
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func getSpawn():
+	# Find a spawning location that is allowed by the bat
+	spawn.x = (randf()*spawnWidth + spawnxLeft)
+	spawn.y = (randf()*spawnHeight + spawnyUp)
+	return spawn
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	#var bat = instance()
-	
-	spawn.x = int(randf()*spawnWidth + spawnxLeft)
-	spawn.y = int(randf()*spawnHeight + spawnyUp)
-	
-	#bat.position.x = spawn.x
-	#bat.position.y = spawn.y
-	#add_child(bat)
+#func _process(delta):
+#	pass
--- Game.gd
+++ Game.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 # Declare member variables here. Examples:
-var enemy = ("ref://Bat.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
