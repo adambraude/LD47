938887861f24625392066bf1ed24335c4c36ab30
adambraude <malfunctioningadamantium@gmail.com>
2020-10-04 12:45:01 -0700

    add player gun

---
+++ Bullet.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+export var velocity = 0
+export var direction = 0
+export var drag = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	move_local_x(velocity*delta*cos(direction))
+	move_local_y(velocity*delta*sin(direction))
+	if (velocity < 0):
+		velocity = 0
+	else:
+		velocity -= delta*drag
+	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+	pass
---
+++ Gun.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+export var bulletType = "res://Bullet-0.tscn"
+var bulletScene
+var shooting = false;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	bulletScene = load(bulletType)
+	$AnimatedSprite.play()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(Input.is_action_pressed("shoot") and $cooldown.time_left == 0):
+		  $AnimatedSprite.animation = "fire"
+		  var bullet = bulletScene.instance()
+		  bullet.position = self.global_position
+		  bullet.direction = PI*3/2
+		  bullet.direction += rand_range(-PI/16, PI/16)
+		  get_node("/root/").add_child(bullet)
+		  $cooldown.start()
+	if(not Input.is_action_pressed("shoot")):
+		$AnimatedSprite.animation = "idle"
+	pass
+
