2ac24fb0bfe0dc35a62002b9ab8e2ad4b7e9ae06
ECorpron <ecorpron@gmail.com>
2020-10-04 22:02:41 -0700

    Spawn now spawns in with health and can be shot

--- Boss1.gd
+++ Boss1.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -9,8 +8,22 @@ extends Area2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite.play("Idle")
+	
+	var player_health = $Health
+	var healthbar = $HealthBar
+	
+	player_health.connect("changed", healthbar, "set_value")
+	player_health.connect("max_changed", healthbar, "set_max")
+	player_health.initialize()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Boss1_area_entered(area):
+	print("boss was hit")
+	$Health.take_damage(1)
+	if area.has_method("die"):
+		area.die()
