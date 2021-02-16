cff43bf07bb3519cecbb2f49b018b2bd973dfd23
Hunter Loftus <31742741+hunterloftus@users.noreply.github.com>
2021-02-15 18:15:49 -0800

    Github Desktop Testing
    Added Comments

--- Background.gd
+++ Background.gd
@@ -4,7 +4,7 @@ extends ParallaxBackground
 # Declare member variables here. Examples:
 # var a = 2
 export var speed = 10
-
+#blablabla
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
